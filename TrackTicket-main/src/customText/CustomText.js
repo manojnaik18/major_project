2f5c5a03021a2cbabf489fb0bcb1a5ab428e8106
manojnaik18 <122071370+manojnaik18@users.noreply.github.com>
2024-12-12 23:27:31 +0530

    Delete TrackTicket-main directory

--- TrackTicket-main/src/customText/CustomText.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Text } from 'react-native-paper';
-
-const CustomText = ({ style, ...props }) => {
-  return <Text  style={[style]} {...props} />;
-};
-
-export default CustomText;
